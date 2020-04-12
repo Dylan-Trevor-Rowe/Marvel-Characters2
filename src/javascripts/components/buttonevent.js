a16b630777854b0b2fd3553567222addaa4b7941
Dylan Trevor Rowe <Dylanthk@gmail.com>
2020-04-12 12:37:38 -0500

    finalproduct

--- src/javascripts/components/buttonevent.js
+++ src/javascripts/components/buttonevent.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import character from "./helpers/data/characterData";
 import characterList from "./characterlist/characterList";
-// functions to filter data by the id and print on the correct cards by button click
+
 const xmenButton = () => {
   document.getElementById("x-Menbtn").addEventListener("click", () => {
     document.getElementById("card").innerHTML = "";
@@ -27,10 +27,6 @@ const guardiansButton = () => {
 const avengersButton = () => {
   document.getElementById("AvengersBtn").addEventListener("click", () => {
     document.getElementById("card").innerHTML = "";
-    // eslint-disable-next-line func-names
-    // document.getElementById("").onclick = function (e) {
-    //   e.target.style.visibility = 'hidden';
-    // };
     const avengersCharacter = character.getCharacters();
     const avengersFilter = avengersCharacter.filter(
       (person) => person.teamId === "team1",
@@ -40,8 +36,8 @@ const avengersButton = () => {
 };
 
 const imageHide = () => {
-  $("AvengersBtn").click(() => {
-    $("#otherImage").hide(1000);
+  $("button").click(() => {
+    $('#otherimage').hide();
   });
 };
 export default {
--- src/javascripts/components/buttons.js
+++ src/javascripts/components/buttons.js
@@ -4,9 +4,9 @@ import print from './helpers/utils';
 const buildButtonEvent = () => {
   let domstring = '';
   domstring += `<div>
-<button type="button1" id="x-Menbtn" class="x-Men">X-men</button>
-<button type="button2" id="guardiansBtn" class="Guardians">Guardians</button>
-<button type="button3" id="AvengersBtn" class="Avengers">Avengers</button>
+<button type="button" id="x-Menbtn" class="btn btn-primary">X-men</button>
+<button type="button" id="guardiansBtn" class="btn btn-primary">Guardians</button>
+<button type="button" id="AvengersBtn" class="btn btn-primary">Avengers</button>
 </div>`;
   print.printToDom('heroButtons', domstring);
 };
